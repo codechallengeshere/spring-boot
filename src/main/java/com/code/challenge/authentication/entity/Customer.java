37732f085295ed8225baf8180a9d4c7251eaa39d
ribalka.aleksandr <ribalka.aleksandr@gmail.com>
2023-12-20 14:31:00 +0200

    - updated customer entity

--- src/main/java/com/code/challenge/authentication/entity/Customer.java
+++ src/main/java/com/code/challenge/authentication/entity/Customer.java
@@ -2,36 +2,69 @@ package com.code.challenge.authentication.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "customers", indexes = {})
+@Table(
+        name = "customers",
+        indexes = {
+                @Index(
+                        name = "index__email",
+                        columnList = "email",
+                        unique = true
+                ),
+                @Index(
+                        name = "index__is_enabled",
+                        columnList = "isEnabled"
+                ),
+                @Index(
+                        name = "index__is_enabled__reset_password_token",
+                        columnList = "isEnabled,resetPasswordToken"
+                )
+        },
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "unique_constraint__email",
+                        columnNames = {
+                                "email"
+                        }
+                )
+        }
+)
 public class Customer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotBlank
+    @Length(max = 50)
     @Email
-    @Column(name = "email", unique = true, nullable = false)
+    @Column(name = "email", unique = true, nullable = false, length = 50)
     private String email;
 
-    @Column(name = "first_name", nullable = false)
+    @NotBlank
+    @Length(max = 50)
+    @Column(name = "first_name", nullable = false, length = 50)
     private String firstName;
 
-    @Column(name = "last_name", nullable = false)
+    @NotBlank
+    @Length(max = 50)
+    @Column(name = "last_name", nullable = false, length = 50)
     private String lastName;
 
     @Column(name = "is_enabled", nullable = false)
     private boolean isEnabled;
 
-    @Column(name = "reset_password_token", unique = true)
+    @Column(name = "reset_password_token", unique = true, length = 255)
     private String resetPasswordToken;
 }
